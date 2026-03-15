3e50cdb31cd6bff78cb15b05548911465648bd0d
Tianyi <0.tianyi.lim@gmail.com>
2026-03-15 18:29:11 +0800

    Add a command-line argument to set correct ROS_DOMAIN_ID

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Check that an argument is specified.
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <RPI_ID>"
+    exit 1
+fi
+RPI_ID="$1"
+
 git submodule update --init --recursive
 
 # Install relevant packages system-wide
@@ -20,3 +27,11 @@ python3 -m pip install --break-system-packages catkin_pkg smbus numpy
 # Build the workspace. The CMake arg ensures that colcon/cmake uses the venv's Python, and not any other python exe from
 # e.g conda.
 colcon build --symlink-install
+
+# Add to .bashrc to auto-source this workspace and to set the correct ROS_DOMAIN_ID
+{
+    echo "source /opt/ros/humble/setup.bash"
+    echo "source $(pwd)/install/setup.bash"
+    echo "export ROS_DOMAIN_ID=$RPI_ID"
+    echo "ROS_DOMAIN_ID is set to $ROS_DOMAIN_ID"
+} >>~/.bashrc
