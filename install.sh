89c6b20a86d428b8696f9ea7b6d04d74142647b6
Tianyi <0.tianyi.lim@gmail.com>
2026-03-15 17:43:36 +0800

    Update install script; no need for venv

--- install.sh
+++ install.sh
@@ -5,24 +5,18 @@ git submodule update --init --recursive
 
 # Install relevant packages system-wide
 sudo apt update
-sudo apt install -y python3-smbus python3-venv net-tools network-manager build-essential cmake
-
-# Create venv if it doesn't exist
-if [ ! -d "venv" ]; then
-    python3 -m venv ./venv
-fi
+sudo apt install -y python3-smbus python3-venv python3-pip \
+    net-tools network-manager build-essential cmake \
+    python3-colcon-common-extensions
 
 # Permissions for I2C and serial
 sudo usermod -aG i2c "$USER"
 sudo usermod -aG dialout "$USER"
 
-# Activate venv
-source ./venv/bin/activate
-
-# Install relevant packages in venv
-python -m pip install --upgrade pip
-python -m pip install colcon_common_extensions catkin_pkg smbus numpy
+# Install relevant pip packages
+python3 -m pip install --upgrade pip --break-system-packages
+python3 -m pip install --break-system-packages catkin_pkg smbus numpy
 
 # Build the workspace. The CMake arg ensures that colcon/cmake uses the venv's Python, and not any other python exe from
 # e.g conda.
-colcon build  --cmake-args -DPython3_FIND_VIRTUALENV="ONLY"
+colcon build --symlink-install
