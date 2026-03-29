2cee840f874094ca7bbf636e2aa64cc9de620a51
Tianyi <0.tianyi.lim@gmail.com>
2026-03-29 20:55:03 +0800

    More pythonic and separable launch description

--- src/wos_bringup/launch/wos_bringup.launch.py
+++ src/wos_bringup/launch/wos_bringup.launch.py
@@ -9,144 +9,184 @@ from launch.actions import LogInfo
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
-# RPLidar A1 constants
-LIDAR_CHANNEL_TYPE = 'serial'
-LIDAR_BAUDRATE = 115200
-LIDAR_IS_INVERTED = False
-LIDAR_TO_ANGLE_COMPENSATE = True
-LIDAR_SCAN_MODE = 'Sensitivity'
+
+# Launch arguments for RPLidar A1
+channel_type = LaunchConfiguration('channel_type', default='serial')
+serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
+serial_baudrate = LaunchConfiguration('serial_baudrate', default='115200')
+frame_id = LaunchConfiguration('frame_id', default='laser')
+inverted = LaunchConfiguration('inverted', default='false')
+angle_compensate = LaunchConfiguration('angle_compensate', default='true')
+scan_mode = LaunchConfiguration('scan_mode', default='Sensitivity')
+
+# Launch arguments for motor driver
+max_linear_vel = LaunchConfiguration('max_linear_vel', default='1.0')
+max_angular_vel = LaunchConfiguration('max_angular_vel', default='2.5')
+left_right_ratio = LaunchConfiguration('left_right_ratio', default='1.0')
+wheelbase = LaunchConfiguration('wheelbase', default='0.12')
+wheel_radius = LaunchConfiguration('wheel_radius', default='0.032')
+wheel_angvel_to_pwm = LaunchConfiguration('wheel_angvel_to_pwm', default='1.0')
+invert_left_motor = LaunchConfiguration('invert_left_motor', default='false')
+invert_right_motor = LaunchConfiguration('invert_right_motor', default='false')
+
+# Launch arguments for IMU driver
+yaw_b_i_deg = LaunchConfiguration('yaw_B_I_deg', default='180.0')
+pitch_b_i_deg = LaunchConfiguration('pitch_B_I_deg', default='0.0')
+roll_b_i_deg = LaunchConfiguration('roll_B_I_deg', default='180.0')
+freq_hz = LaunchConfiguration('freq_hz', default='100.0')
 
 
-def generate_launch_description():
-    # Launch arguments for RPLidar A1
-    lidar_serial_port = LaunchConfiguration('serial_port')
-    lidar_frame_id = LaunchConfiguration('frame_id')
+def get_lidar_launch_arguments():
+    return [
+        DeclareLaunchArgument(
+            'channel_type',
+            default_value=channel_type,
+            description='Specifying channel type of lidar'),
 
-    # Launch arguments for motor driver
-    max_linear_vel = LaunchConfiguration('max_linear_vel')
-    max_angular_vel = LaunchConfiguration('max_angular_vel')
-    left_right_ratio = LaunchConfiguration('left_right_ratio')
-    wheelbase = LaunchConfiguration('wheelbase')
-    wheel_radius = LaunchConfiguration('wheel_radius')
-    wheel_angvel_to_pwm = LaunchConfiguration('wheel_angvel_to_pwm')
-    invert_left_motor = LaunchConfiguration('invert_left_motor')
-    invert_right_motor = LaunchConfiguration('invert_right_motor')
-
-    # Launch arguments for IMU driver
-    yaw_b_i_deg = LaunchConfiguration('yaw_B_I_deg')
-    pitch_b_i_deg = LaunchConfiguration('pitch_B_I_deg')
-    roll_b_i_deg = LaunchConfiguration('roll_B_I_deg')
-    freq_hz = LaunchConfiguration('freq_hz')
-
-    return LaunchDescription([
         DeclareLaunchArgument(
             'serial_port',
-            default_value='/dev/ttyUSB0',
+            default_value=serial_port,
             description='Specifying usb port to connected lidar'),
 
         DeclareLaunchArgument(
-            'frame_id',
-            default_value='laser',
-            description='Specifying TF frame_id of lidar'),
+            'serial_baudrate',
+            default_value=serial_baudrate,
+            description='Specifying usb port baudrate to connected lidar'),
 
         DeclareLaunchArgument(
+            'frame_id',
+            default_value=frame_id,
+            description='Specifying frame_id of lidar'),
+
+        DeclareLaunchArgument(
+            'inverted',
+            default_value=inverted,
+            description='Specifying whether or not to invert scan data'),
+
+        DeclareLaunchArgument(
+            'angle_compensate',
+            default_value=angle_compensate,
+            description='Specifying whether or not to enable angle_compensate of scan data'),
+        DeclareLaunchArgument(
+            'scan_mode',
+            default_value=scan_mode,
+            description='Specifying scan mode of lidar'),
+    ]
+
+
+def get_motor_driver_launch_arguments():
+    return [
+        DeclareLaunchArgument(
             'max_linear_vel',
-            default_value='1.0',
+            default_value=max_linear_vel,
             description='Maximum linear velocity for motor driver (m/s)'),
 
         DeclareLaunchArgument(
             'max_angular_vel',
-            default_value='2.5',
+            default_value=max_angular_vel,
             description='Maximum angular velocity for motor driver (rad/s)'),
 
         DeclareLaunchArgument(
             'left_right_ratio',
-            default_value='1.0',
+            default_value=left_right_ratio,
             description='Left-to-right motor speed ratio compensation'),
 
         DeclareLaunchArgument(
             'wheelbase',
-            default_value='0.12',
+            default_value=wheelbase,
             description='Distance between left and right wheel centers (m)'),
 
         DeclareLaunchArgument(
             'wheel_radius',
-            default_value='0.032',
+            default_value=wheel_radius,
             description='Wheel radius (m)'),
 
         DeclareLaunchArgument(
             'wheel_angvel_to_pwm',
-            default_value='20.0',
+            default_value=wheel_angvel_to_pwm,
             description='Conversion factor from wheel angular velocity to PWM'),
 
         DeclareLaunchArgument(
             'invert_left_motor',
-            default_value='false',
+            default_value=invert_left_motor,
             description='Whether to invert the left motor direction'),
 
         DeclareLaunchArgument(
             'invert_right_motor',
-            default_value='false',
+            default_value=invert_right_motor,
             description='Whether to invert the right motor direction'),
+    ]
 
+
+def get_imu_driver_launch_arguments():
+    return [
         DeclareLaunchArgument(
             'yaw_B_I_deg',
-            default_value='180.0',
+            default_value=yaw_b_i_deg,
             description='Yaw rotation from IMU frame to robot body frame (deg)'),
 
         DeclareLaunchArgument(
             'pitch_B_I_deg',
-            default_value='0.0',
+            default_value=pitch_b_i_deg,
             description='Pitch rotation from IMU frame to robot body frame (deg)'),
 
         DeclareLaunchArgument(
             'roll_B_I_deg',
-            default_value='180.0',
+            default_value=roll_b_i_deg,
             description='Roll rotation from IMU frame to robot body frame (deg)'),
 
         DeclareLaunchArgument(
             'freq_hz',
-            default_value='50.0',
+            default_value=freq_hz,
             description='IMU polling frequency (Hz)'),
+    ]
 
-        Node(
-            package='rplidar_ros',
-            executable='rplidar_node',
-            name='rplidar_node',
-            parameters=[{'channel_type': LIDAR_CHANNEL_TYPE,
-                         'serial_port': lidar_serial_port,
-                         'serial_baudrate': LIDAR_BAUDRATE,
-                         'frame_id': lidar_frame_id,
-                         'inverted': LIDAR_IS_INVERTED,
-                         'angle_compensate': LIDAR_TO_ANGLE_COMPENSATE,
-                         'scan_mode': LIDAR_SCAN_MODE}],
-            output='screen'),
 
-        Node(
-            package='motor_driver',
-            executable='motor_driver_node',
-            name='motor_driver_node',
-            parameters=[{
-                'max_linear_vel': max_linear_vel,
-                'max_angular_vel': max_angular_vel,
-                'left_right_ratio': left_right_ratio,
-                'wheelbase': wheelbase,
-                'wheel_radius': wheel_radius,
-                'wheel_angvel_to_pwm': wheel_angvel_to_pwm,
-                'invert_left_motor': invert_left_motor,
-                'invert_right_motor': invert_right_motor,
-            }],
-            output='screen'),
+def generate_launch_description():
 
-        Node(
-            package='imu_driver',
-            executable='imu_driver_node',
-            name='imu_driver_node',
-            parameters=[{
-                'yaw_B_I_deg': yaw_b_i_deg,
-                'pitch_B_I_deg': pitch_b_i_deg,
-                'roll_B_I_deg': roll_b_i_deg,
-                'freq_hz': freq_hz,
-            }],
-            output='screen'),
-    ])
+    return LaunchDescription(
+        get_lidar_launch_arguments() +
+        get_motor_driver_launch_arguments() +
+        get_imu_driver_launch_arguments() +
+        [
+            Node(
+                package='rplidar_ros',
+                executable='rplidar_node',
+                name='rplidar_node',
+                parameters=[{'channel_type': channel_type,
+                             'serial_port': serial_port,
+                             'serial_baudrate': serial_baudrate,
+                             'frame_id': frame_id,
+                             'inverted': inverted,
+                             'angle_compensate': angle_compensate,
+                             'scan_mode': scan_mode}],
+                output='screen'),
+
+            Node(
+                package='motor_driver',
+                executable='motor_driver_node',
+                name='motor_driver_node',
+                parameters=[{
+                    'max_linear_vel': max_linear_vel,
+                    'max_angular_vel': max_angular_vel,
+                    'left_right_ratio': left_right_ratio,
+                    'wheelbase': wheelbase,
+                    'wheel_radius': wheel_radius,
+                    'wheel_angvel_to_pwm': wheel_angvel_to_pwm,
+                    'invert_left_motor': invert_left_motor,
+                    'invert_right_motor': invert_right_motor,
+                }],
+                output='screen'),
+
+            Node(
+                package='imu_driver',
+                executable='imu_driver_node',
+                name='imu_driver_node',
+                parameters=[{
+                    'yaw_B_I_deg': yaw_b_i_deg,
+                    'pitch_B_I_deg': pitch_b_i_deg,
+                    'roll_B_I_deg': roll_b_i_deg,
+                    'freq_hz': freq_hz,
+                }],
+                output='screen'),
+        ])
