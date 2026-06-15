fee13e4153e9160e24e608de36251cf8676de2cd
Tian Yi Lim <ltianyi@dso.org.sg>
2026-06-15 17:44:42 +0800

    Update sanitised code base

--- src/motor_driver/motor_driver/motor_driver_node_sanitised.py
+++ src/motor_driver/motor_driver/motor_driver_node_sanitised.py
@@ -1,62 +1,58 @@
 from rcl_interfaces.msg import ParameterDescriptor
 import rclpy
 from rclpy.node import Node
-import time
-from pprint import pformat
-from geometry_msgs.msg import Twist, Vector3
-from motor_driver.motor_driver_impl import THIS_BOARD_TYPE, DFRobot_DC_Motor_IIC as Board
+from geometry_msgs.msg import Twist
+
+import RPi.GPIO as GPIO
+
+# Set up. You can consult the pinout.xyz website to change the pins if needed,
+# but make sure to update the wiring accordingly.
+
+MOTOR_1_OUT_PIN_A = 32
+MOTOR_1_OUT_PIN_B = 36
+MOTOR_2_OUT_PIN_A = 38
+MOTOR_2_OUT_PIN_B = 40
+
+# Physical pulse frequency for the motors.
+PULSE_FREQ = 50
+
+# Initialise GPIO pins
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(MOTOR_1_OUT_PIN_A, GPIO.OUT)
+GPIO.setup(MOTOR_1_OUT_PIN_B, GPIO.OUT)
+GPIO.setup(MOTOR_2_OUT_PIN_A, GPIO.OUT)
+GPIO.setup(MOTOR_2_OUT_PIN_B, GPIO.OUT)
 
 
-def print_board_status(board: Board):
-    '''Helper function to read Motor Driver board status.'''
-    if board.last_operate_status == board.STA_OK:
-        print("board status: everything ok")
-    elif board.last_operate_status == board.STA_ERR:
-        print("board status: unexpected error")
-    elif board.last_operate_status == board.STA_ERR_DEVICE_NOT_DETECTED:
-        print("board status: device not detected")
-    elif board.last_operate_status == board.STA_ERR_PARAMETER:
-        print("board status: parameter error, last operate no effective")
-    elif board.last_operate_status == board.STA_ERR_SOFT_VERSION:
-        print("board status: unsupport board framware version")
+def _move_motor(pin_a, pin_b,
+                is_forward: bool = True,
+                speed: float = 0):
 
+    speed = min(100, max(0, speed))
 
-def get_motor_driver_board(node: "MotorDriverNode") -> Board:
-    '''Helper function to read Motor Driver board status.'''
-    if THIS_BOARD_TYPE:
-        # RaspberryPi select bus 1, set address to 0x10
-        board = Board(1, 0x10)
+    # TODO: With knowledge of how a dual H-bridge motor driver works, implement the logic to control
+    #      forward and reverse motion of motors using the two pins.
+    # Change the assignment of these values below.
+    pin_a_speed = None
+    pin_b_speed = None
+
+    if is_forward:
+        raise NotImplementedError(
+            "This function is not implemented yet. Please implement the motor control logic here.")
+        pin_a.ChangeDutyCycle(pin_a_speed)
+        pin_b.ChangeDutyCycle(pin_b_speed)
     else:
-        # RockPi select bus 7, set address to 0x10
-        board = Board(7, 0x10)
-
-    # Do initial configuration and hardware sanity check
-    l = board.detect()
-    node.get_logger().info("Motor driver board list conform:")
-    node.get_logger().info(f"{pformat(l)}")
-
-    while board.begin() != board.STA_OK:    # Board begin and check board status
-        print_board_status(board)
-        node.get_logger().warning("Motor driver board init failed, retrying...")
-        time.sleep(2)
-    node.get_logger().info("Motor driver board init success")
-
-    # Set initial parameters
-    # board.set_encoder_enable(board.NONE)                 # Set selected DC motor encoder enable
-    # Set selected DC motor encoder disable
-    board.set_encoder_disable(board.ALL)
-    # board.set_encoder_reduction_ratio(board.ALL, 43)    # Set selected DC motor encoder reduction ratio, test motor reduction ratio is 43.8
-    # Set DC motor pwm frequency to 1000HZ; can experiment with other values.
-    board.set_motor_pwm_frequency(1000)
-    return board
+        raise NotImplementedError(
+            "This function is not implemented yet. Please implement the motor control logic here.")
+        pin_a.ChangeDutyCycle(pin_a_speed)
+        pin_b.ChangeDutyCycle(pin_b_speed)
 
 
 class MotorDriverNode(Node):
+
     def __init__(self):
         super().__init__('motor_driver_node')
 
-        # Declare parameters. This allows us to change values in our ROS Node
-        # without needing to edit the code.
         self.declare_parameter("max_linear_vel", 1.0,
                                descriptor=ParameterDescriptor(description="Maximum linear velocity"))
         self.declare_parameter("max_angular_vel", 0.5,
@@ -84,7 +80,6 @@ class MotorDriverNode(Node):
         self.LEFT_RIGHT_RATIO = self.get_parameter(
             "left_right_ratio").get_parameter_value().double_value
         '''Ratio of left to right motor speed, to account for hardware differences'''
-
         self.WHEELBASE = self.get_parameter(
             "wheelbase").get_parameter_value().double_value
         self.WHEEL_RADIUS = self.get_parameter(
@@ -92,17 +87,23 @@ class MotorDriverNode(Node):
         # Conversion factor from wheel angular velocity (rad/s) to PWM value
         self.WHEEL_ANGVEL_TO_PWM = self.get_parameter(
             "wheel_angvel_to_pwm").get_parameter_value().double_value
-
         self.INVERT_LEFT_MOTOR = self.get_parameter(
             "invert_left_motor").get_parameter_value().bool_value
         self.INVERT_RIGHT_MOTOR = self.get_parameter(
             "invert_right_motor").get_parameter_value().bool_value
 
-        # Initialize motor driver board
-        self.board = get_motor_driver_board(self)
+        # Define motor control pins as class attributes, and set up PWM control on those pins.
+        self.motor_1_a = GPIO.PWM(MOTOR_1_OUT_PIN_A, PULSE_FREQ)
+        self.motor_1_b = GPIO.PWM(MOTOR_1_OUT_PIN_B, PULSE_FREQ)
+        self.motor_2_a = GPIO.PWM(MOTOR_2_OUT_PIN_A, PULSE_FREQ)
+        self.motor_2_b = GPIO.PWM(MOTOR_2_OUT_PIN_B, PULSE_FREQ)
+        self.motor_1_a.start(0)
+        self.motor_1_b.start(0)
+        self.motor_2_a.start(0)
+        self.motor_2_b.start(0)
 
-        # Ensure that motors are stopped at startup
-        self.board.motor_stop(self.board.ALL)
+        # Stop Motor
+        self.stop_motors()
 
         # Initialise callbacks last, after all setup is done
         self.twist_subscription = self.create_subscription(
@@ -110,6 +111,21 @@ class MotorDriverNode(Node):
             'cmd_vel',
             self.twist_callback,
             10)
+
+    def stop_motors(self):
+        '''Convenience function for you to stop the motors by setting the speed to 0.'''
+        _move_motor(self.motor_1_a, self.motor_1_b, speed=0.0)
+        _move_motor(self.motor_2_a, self.motor_2_b, speed=0.0)
+
+    def _unbind_gpio(self):
+        '''This function is required to clean up the GPIO pins when the node is stopped,
+        otherwise the pins will remain in their last state and may cause issues when you try to run the node again.'''
+        self.motor_1_a.stop()
+        self.motor_1_b.stop()
+        self.motor_2_a.stop()
+        self.motor_2_b.stop()
+        GPIO.cleanup([MOTOR_1_OUT_PIN_A, MOTOR_1_OUT_PIN_B,
+                      MOTOR_2_OUT_PIN_A, MOTOR_2_OUT_PIN_B])
 
     def twist_callback(self, msg: Twist):
         """
@@ -121,39 +137,39 @@ class MotorDriverNode(Node):
         raw_angular = msg.angular.z
 
         # TODO Step 1: Clamp Linear and Angular Velocities between limits
-        # clamped_linear = 
+        # clamped_linear =
         # clamped_angular =
 
         # TODO Step 2: Calculate the Differential Drive Kinematics
-        ## Convert the Velocity command into the speed for each wheel
+        # Convert the Velocity command into the speed for each wheel
 
         # TODO Step 3: Ensure that Both Motors are spinning at the same speed
-        ## If the Motors do not spin at the same speed, the Robot will not be able to
-        ## drive in a straight line.
+        # If the Motors do not spin at the same speed, the Robot will not be able to
+        # drive in a straight line.
 
         # TODO Step 4: Convert from rad/s to pwm value
-        pwm_left = 0.0 # should be something between 0.0 to 100.0
-        pwm_right = 0.0 # should be something between 0.0 to 100.0
+        pwm_left = 0.0  # should be something between 0.0 to 100.0
+        pwm_right = 0.0  # should be something between 0.0 to 100.0
 
         # TODO Step 4.1 (Optional): Log the Mapping from rad/s to PWM Value for Debug Purposes
-        self.get_logger().info(f"Hello, this a number with value {1.2}")
+        self.get_logger().info(
+            f"Hello, this a number with an example value {1.2}")
 
         # TODO Step 5: Sending the Motor PWM Values to the actual motors
-        ## Specify:
-        ##     - Which Motor to Control (M1 for Left, M2 for Right)
-        ##     - Direction: Clockwise (CW) / Counter Clockwise (CCW)
-        ##     - Speed: PWM Values that you have previously calculated above
+        # Specify:
+        # - Which Motor to Control (M1 for Left, M2 for Right)
+        # - Direction: Clockwise (CW) / Counter Clockwise (CCW)
+        # - Speed: PWM Values that you have previously calculated above
 
         # Set motor directions and speeds. For instance:
-        left_motor_direction = self.board.CW   # TODO: what should this be?
-        right_motor_direction = self.board.CCW # TODO: what should this be?
+        left_motor_is_forward = True  # or False, depending on the sign of the speed
+        right_motor_is_forward = True  # or False, depending on the sign of the speed
 
-        # Write to left motor
-        self.board.motor_movement(
-            [self.board.M1], left_motor_direction, pwm_left)
-        # Write to right motor
-        self.board.motor_movement(
-            [self.board.M2], right_motor_direction, pwm_right)
+        # Call motor control function with the appropriate parameters. For instance:
+        _move_motor(self.motor_1_a, self.motor_1_b,
+                    is_forward=left_motor_is_forward, speed=pwm_left)
+        _move_motor(self.motor_2_a, self.motor_2_b,
+                    is_forward=right_motor_is_forward, speed=pwm_right)
 
 
 def main(args=None):
@@ -164,15 +180,15 @@ def main(args=None):
     except KeyboardInterrupt:
         print("[motor driver]:", "-"*100)
         print("[motor driver]: STOPPING MOTOR DRIVER")
-        motor_driver.board.motor_movement(
-            [motor_driver.board.M1, motor_driver.board.M2],
-            motor_driver.board.CW, 0)
+        motor_driver.stop_motors()
+        motor_driver._unbind_gpio()
         print("[motor driver]:", "-"*100)
     except Exception as e:
         print(f"[motor driver]: {e}")
 
     motor_driver.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
